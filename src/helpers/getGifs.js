872b81ae72ed55f8c1a1183f663bbb65b8ee96e4
edu82 <eduardo@philipsoft.cl>
2022-12-17 15:14:33 -0300

    Firzt Kommit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async (category) => {
+    const apiKey = 'VgU5400mo0CyCQWfPe0QUm9XxdpWpyE6'
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`
+    const resp = await fetch(url)
+    const { data } = await resp.json()
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+    console.log(gifs);
+    return gifs
+}
